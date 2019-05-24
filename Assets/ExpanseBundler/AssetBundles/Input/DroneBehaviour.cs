03adbf19ae32f35b61ac10a6e0692dd580816f58
Shane Harris <shane@xactaccounts.co.uk>
2019-05-24 21:59:02 +0100

    initial commit

---
+++ Assets/ExpanseBundler/AssetBundles/Input/DroneBehaviour.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DroneBehaviour : ExpanseBehaviour {
+    public Rigidbody Drone;
+    private Vector3 DroneRotation;
+    private Vector3 torque = Vector3.zero;
+    private float translateOffset = 0;
+    private float rotateOffset = 0;
+    private Transform RightHand;
+    private OVRInput.Controller controller;
+
+    void Start () {
+        var go = transform.Find("Container/DronePrefab").gameObject;
+        Drone = go.GetComponent<Rigidbody>();
+        List<GameObject> rootObjects = new List<GameObject>();
+        Scene scene = SceneManager.GetActiveScene();
+        scene.GetRootGameObjects(rootObjects);
+        for (int i = 0; i < rootObjects.Count; i++) {
+            if (rootObjects[i].name == "Cockpit") {
+                RightHand = rootObjects[i].transform.Find("OVRCameraRig/TrackingSpace/RightHandAnchor");
+            }
+        }
+        controller = OVRInput.Controller.RTouch | OVRInput.Controller.RTrackedRemote;
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        DroneRotation = Drone.transform.localEulerAngles;
+
+        if (DroneRotation.z > 10 && DroneRotation.z <= 180) { Drone.AddRelativeTorque(0, 0, -20); }//if tilt too big(stabilizes drone on z-axis)
+        if (DroneRotation.z > 180 && DroneRotation.z <= 350) { Drone.AddRelativeTorque(0, 0, 20); }//if tilt too big(stabilizes drone on z-axis)
+        if (DroneRotation.z > 1 && DroneRotation.z <= 10) { Drone.AddRelativeTorque(0, 0, -3); }//if tilt not very big(stabilizes drone on z-axis)
+        if (DroneRotation.z > 350 && DroneRotation.z < 359) { Drone.AddRelativeTorque(0, 0, 3); }//if tilt not very big(stabilizes drone on z-axis)
+
+        if (DroneRotation.x > 10 && DroneRotation.x <= 180) { Drone.AddRelativeTorque(-20, 0, 0); }//if tilt too big(stabilizes drone on x-axis)
+        if (DroneRotation.x > 180 && DroneRotation.x <= 350) { Drone.AddRelativeTorque(20, 0, 0); }//if tilt too big(stabilizes drone on x-axis)
+        if (DroneRotation.x > 1 && DroneRotation.x <= 10) { Drone.AddRelativeTorque(-3, 0, 0); }//if tilt not very big(stabilizes drone on x-axis)
+        if (DroneRotation.x > 350 && DroneRotation.x < 359) { Drone.AddRelativeTorque(3, 0, 0); }//if tilt not very big(stabilizes drone on x-axis)  
+
+        bool rightTriggerDown = OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, controller);
+        bool rightTrigger = OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger, controller);
+        if (rightTriggerDown) {
+            translateOffset = RightHand.position.y;
+            rotateOffset = RightHand.eulerAngles.y;
+        }
+        if (rightTrigger) {
+            Drone.AddRelativeForce(0, (RightHand.position.y - translateOffset) * 10, 0);
+            var rotY = RightHand.eulerAngles.y - rotateOffset;
+            torque.z = RightHand.eulerAngles.z >= 180 && RightHand.eulerAngles.z < 360 ? Mathf.Clamp(-(360 - RightHand.eulerAngles.z), -45, 0) : Mathf.Clamp(RightHand.eulerAngles.z, 0, 45);
+            torque.y = rotY >= 180 && rotY < 360 ? Mathf.Clamp(-(360 - rotY), -45, 0) : Mathf.Clamp(rotY, 0, 45);
+            torque.x = RightHand.eulerAngles.x >= 180 && RightHand.eulerAngles.x < 360 ? Mathf.Clamp(-(360 - RightHand.eulerAngles.x), -45, 0) : Mathf.Clamp(RightHand.eulerAngles.x, 0, 45);
+            Drone.AddRelativeTorque(torque.z / 2.25f, torque.y, torque.x / 2.25f);
+            Drone.AddForce(0, 9, 0);//9.80665f
+        }
+    }
+}
---
+++ Assets/ExpanseBundler/Editor/Bundle.cs
@@ -0,0 +1,100 @@
+﻿using UnityEditor;
+using System.IO;
+using System;
+using UnityEngine;
+
+public class Bundle : EditorWindow {
+
+    string inputDirectory = "Assets/ExpanseBundler/AssetBundles/Input";
+    string outputDirectory = "Assets/ExpanseBundler/AssetBundles/Output";
+    string outputName = "my_bundle";
+    bool deleteManifests = true;
+
+    public Texture tex;
+
+    void OnGUI() {
+        minSize = new Vector2(350, 288);
+        var centeredStyle = GUI.skin.GetStyle("Label");
+        centeredStyle.alignment = TextAnchor.UpperCenter;
+        GUILayout.Label(tex, centeredStyle);
+        GUILayout.FlexibleSpace();
+        GUILayout.MinHeight(400f);
+        GUILayout.Label("Expanse Asset Bundle Settings", EditorStyles.boldLabel);
+        GUILayout.Label("Enter the below settings to generate your asset bundles.\nWARNING: Items in the output folder may be overwritten!", EditorStyles.wordWrappedLabel);
+        GUILayout.Space(20);
+        inputDirectory = EditorGUILayout.TextField("Input Directory", inputDirectory);
+        outputDirectory = EditorGUILayout.TextField("Output Directory", outputDirectory);
+        outputName = EditorGUILayout.TextField("Output Name", outputName);
+        deleteManifests = EditorGUILayout.Toggle("Remove Manifests", deleteManifests);
+        GUILayout.Space(10);
+        if (GUILayout.Button("Build")) {
+            BuildAllAssetBundles();
+        }
+    }
+    
+    [MenuItem("Tools/ExpanseBundler")]
+    public static void ShowWindow() {
+        EditorWindow.GetWindow(typeof(Bundle));
+    }
+    
+    void BuildAllAssetBundles() {
+        if (!Directory.Exists(inputDirectory)) {
+            EditorUtility.DisplayDialog("No Input", "The input directory does not exist!", "OK");
+            return;
+        }
+       // BuildReferences.ResetReferences();
+        foreach (string fileName in Directory.GetFiles(inputDirectory)) {
+            switch (Path.GetExtension(fileName)) {
+                case ".cs":
+                    string newFileName = fileName.Substring(0, fileName.Length - Path.GetFileName(fileName).Length) + Path.GetFileNameWithoutExtension(fileName) + ".cs.txt";
+                    File.Copy(fileName, newFileName);
+                    break;
+                case ".prefab":
+                   // BuildReferences.Scan(fileName); //inputDirectory + "/" + 
+                    break;
+            }
+        }
+
+        AssetDatabase.Refresh();
+
+        BuildBundle(BuildTarget.StandaloneWindows);
+        string[] fileEntries = BuildBundle(BuildTarget.Android);
+
+        foreach (string fileName in fileEntries) {
+            if (fileName.Length > 7 && fileName.Substring(fileName.Length - 7) == ".cs.txt") {
+                File.Delete(fileName);
+            }
+        }
+        if (deleteManifests) {
+            string[] newFileEntries = Directory.GetFiles(outputDirectory);
+            foreach (string fileName in newFileEntries) {
+                if (fileName.Substring(fileName.Length - 9) == ".manifest" ||
+                    Path.GetFileName(fileName) == "Output" ||
+                    Path.GetFileName(fileName) == "Output.meta" ||
+                    fileName.Substring(fileName.Length - 14) == ".manifest.meta") {
+                    File.Delete(fileName);
+                }
+            }
+        }
+        Selection.activeObject = AssetDatabase.LoadMainAssetAtPath(outputDirectory + "/" + outputName);
+        EditorGUIUtility.PingObject(Selection.activeObject);
+        AssetDatabase.Refresh();
+    }
+
+    string[] BuildBundle(BuildTarget target) {
+        string[] fileEntries = Directory.GetFiles(inputDirectory);
+        foreach (string fileName in fileEntries) {
+            if (Path.GetExtension(fileName) != ".cs") {
+                AssetImporter asset = AssetImporter.GetAtPath(fileName);
+                if (asset != null) {
+                    asset.SetAssetBundleNameAndVariant(target == BuildTarget.StandaloneWindows ? outputName : outputName + "_android", "");
+                }
+            }
+        }
+        if (!Directory.Exists(outputDirectory)) {
+            Directory.CreateDirectory(outputDirectory);
+        }
+        BuildPipeline.BuildAssetBundles(outputDirectory, BuildAssetBundleOptions.None, target);
+        return fileEntries;
+    }
+}
